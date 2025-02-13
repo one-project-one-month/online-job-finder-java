eb8074199cd6bd0b986a531292849b298f9d70bd
coderAung <yewontaung798@gmail.com>
2025-02-13 23:35:51 +0630

    add a custom method to BaseRepository

--- src/main/java/com/opom/jobfinder/model/repo/BaseRepository.java
+++ src/main/java/com/opom/jobfinder/model/repo/BaseRepository.java
@@ -1,10 +1,17 @@
 package com.opom.jobfinder.model.repo;
 
+import java.util.List;
+import java.util.function.Function;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
 
 @NoRepositoryBean
 public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
 
+	<DTO> List<DTO> search(Function<CriteriaBuilder, CriteriaQuery<DTO>> queryFunc);
 	
 }
--- src/main/java/com/opom/jobfinder/model/repo/account/AccountRepo.java
+++ src/main/java/com/opom/jobfinder/model/repo/account/AccountRepo.java
@@ -1,9 +1,9 @@
 package com.opom.jobfinder.model.repo.account;
 
-import com.opom.jobfinder.model.entity.account.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
 
-public interface AccountRepo extends JpaRepository<Account, UUID> {
+import com.opom.jobfinder.model.entity.account.Account;
+import com.opom.jobfinder.model.repo.BaseRepository;
+
+public interface AccountRepo extends BaseRepository<Account, UUID> {
 }
