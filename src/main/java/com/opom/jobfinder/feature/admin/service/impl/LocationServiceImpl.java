0cb731ead2b1bea2d8809c7523393ffb74cfd866
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-15 08:57:52 +0630

    override  methods in LocationServiceImpl(not finished yet)

--- src/main/java/com/opom/jobfinder/feature/admin/service/impl/LocationServiceImpl.java
+++ src/main/java/com/opom/jobfinder/feature/admin/service/impl/LocationServiceImpl.java
@@ -12,7 +12,64 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class LocationServiceImpl  {
+public class LocationServiceImpl implements LocationService {
 
+    // CONSTANT VALUES
+    private final LocationRepo locationRepo;
+
+    // CONSTRUCTOR
+    public LocationServiceImpl(LocationRepo locationRepo) {
+        this.locationRepo = locationRepo;
+    }
+
+    @Override
+    public Location createLocation(Location location) {
+        return locationRepo.save(location);
+    }
+
+    @Override
+    public List<Location> getAllLocations() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Location> getLocationById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Location updateLocation(Location locationDetails) {
+        return null;
+    }
+
+    @Override
+    public void deleteLocation(String id) {
+
+    }
+
+    @Override
+    public Location updateLocationByApplicant(String locationId, String applicantId) {
+        return null;
+    }
+
+    @Override
+    public Location updateLocationByCompany(String locationId, String applicantId) {
+        return null;
+    }
+
+    @Override
+    public List<Applicant> getApplicantsByLocationId(String locationId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Job> getJobsByLocation(String locationID) {
+        return List.of();
+    }
+
+    @Override
+    public List<Company> getCompaniesByLocation(String locationId) {
+        return List.of();
+    }
 
 }
