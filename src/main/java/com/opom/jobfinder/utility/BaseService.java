61706b7239f33a98098d782509f23aa4ba38eb84
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-15 18:27:30 +0630

    implement search method

--- src/main/java/com/opom/jobfinder/utility/BaseService.java
+++ src/main/java/com/opom/jobfinder/utility/BaseService.java
@@ -4,4 +4,9 @@ public abstract class BaseService {
     protected BaseResponse successResponse (Object object) {
         return BaseResponse.of(MessageConstants.SUCCESS, object, Translator.toLocale(MessageConstants.SUCCESS));
     }
+
+    protected BaseResponse errorResponse(String errorCode, String message) {
+        return new BaseResponse(errorCode, null, Translator.toLocale(message));
+    }
+
 }
