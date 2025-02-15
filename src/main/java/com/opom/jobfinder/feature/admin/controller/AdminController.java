dcbeb61099aa0a611f816892bfa0c1691f1066eb
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-15 08:13:44 +0630

    create LocationService class and LocationServiceImpl

--- src/main/java/com/opom/jobfinder/feature/admin/controller/AdminController.java
+++ src/main/java/com/opom/jobfinder/feature/admin/controller/AdminController.java
@@ -1,4 +1,11 @@
 package com.opom.jobfinder.feature.admin.controller;
 
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/admin")
 public class AdminController {
+
 }
---
+++ src/main/java/com/opom/jobfinder/feature/admin/service/impl/LocationServiceImpl.java
@@ -0,0 +1,7 @@
+package com.opom.jobfinder.feature.admin.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationServiceImpl  {
+}
