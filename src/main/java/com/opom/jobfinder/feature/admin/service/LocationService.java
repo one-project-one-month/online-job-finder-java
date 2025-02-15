1f8d1e706e8830082fd7e9a46f98240e1362945f
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-15 08:35:16 +0630

    implement code in LocationService and Modify Id datatype in Lcoation

--- src/main/java/com/opom/jobfinder/feature/admin/service/LocationService.java
+++ src/main/java/com/opom/jobfinder/feature/admin/service/LocationService.java
@@ -1,5 +1,15 @@
 package com.opom.jobfinder.feature.admin.service;
 
-public interface LocationService {
+import com.opom.jobfinder.model.entity.location.Location;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface LocationService {
+    Location createLocation(Location location);
+    List<Location> getAllLocations();
+    Optional<Location> getLocationById(String id);
+    Location updateLocation(String id, Location locationDetails);
+    void deleteLocation(String id);
+    Location updateLocationByApplicatn(String id);
 }
--- src/main/java/com/opom/jobfinder/model/repo/location/LocationRepo.java
+++ src/main/java/com/opom/jobfinder/model/repo/location/LocationRepo.java
@@ -1,4 +1,11 @@
 package com.opom.jobfinder.model.repo.location;
 
-public class LocationRepo {
+import com.opom.jobfinder.model.entity.location.Location;
+import com.opom.jobfinder.model.repo.BaseRepository;
+import com.opom.jobfinder.model.repo.BaseRepositoryImpl;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocationRepo extends BaseRepository<Location, String> {
+
 }
