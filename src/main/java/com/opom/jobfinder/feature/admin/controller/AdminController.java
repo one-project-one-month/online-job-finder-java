d2e4ff775bb8badc4e90852946e671b9d3659ad5
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-16 01:37:41 +0630

    create add Location method in controller

--- src/main/java/com/opom/jobfinder/feature/admin/controller/AdminController.java
+++ src/main/java/com/opom/jobfinder/feature/admin/controller/AdminController.java
@@ -1,11 +1,26 @@
 package com.opom.jobfinder.feature.admin.controller;
 
+import com.opom.jobfinder.feature.admin.service.LocationService;
+import com.opom.jobfinder.model.entity.info.Location;
+import com.opom.jobfinder.utility.BaseResponse;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("admin")
 public class AdminController {
 
+    // CONSTANT VALUES
+    private final LocationService locationService;
+
+    // CONSTRUCTOR
+    public AdminController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BaseResponse> addLocation(@RequestBody Location location ) {
+        return ResponseEntity.ok(locationService.createLocation(location));
+    }
 }
