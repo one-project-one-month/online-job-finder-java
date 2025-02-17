b46277875a74f6188545272435a21057d5437e0c
Coral-Farmiga23 <thandaroo.npe2020@gmail.com>
2025-02-17 23:21:12 +0630

    first commit

--- src/main/java/com/opom/jobfinder/feature/admin/service/AdminService.java
+++ src/main/java/com/opom/jobfinder/feature/admin/service/AdminService.java
@@ -1,4 +1,15 @@
 package com.opom.jobfinder.feature.admin.service;
 
-public interface AdminService {
+import com.opom.jobfinder.dto.info.SkillDto;
+
+import java.util.List;
+
+public interface AdminService{
+
+    List<SkillDto> listAllSkills();
+    SkillDto getSkillById(int id);
+    SkillDto createSkill(SkillDto skillDto);
+    SkillDto updateSkill(SkillDto skillDto, int id);
+    void deleteSkill(int id);
+
 }
