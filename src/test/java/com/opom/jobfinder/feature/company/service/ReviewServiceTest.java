36802b610bd6eb040fe4c42d04dcc4b61880bc49
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-17 21:26:10 +0630

    add Revice Service Test and add Instancio dependency

---
+++ src/test/java/com/opom/jobfinder/feature/company/service/ReviewServiceTest.java
@@ -0,0 +1,30 @@
+package com.opom.jobfinder.feature.company.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith()
+class ReviewServiceTest {
+
+    @Test
+    void addReview() {
+    }
+
+    @Test
+    void updateReview() {
+    }
+
+    @Test
+    void deleteReview() {
+    }
+
+    @Test
+    void getReviewsByCompany() {
+    }
+
+    @Test
+    void getAverageReviewFromCompany() {
+    }
+}
