c900a58f032d5dc1313fb72e157698ad11591299
ahkar <ahkarminhtut111@gmail.com>
2025-02-17 21:49:40 +0630

    change to record and add validation errors

--- src/main/java/com/opom/jobfinder/config/JobFinderSecurityConfig.java
+++ src/main/java/com/opom/jobfinder/config/JobFinderSecurityConfig.java
@@ -1,4 +1,44 @@
 package com.opom.jobfinder.config;
 
+import com.opom.jobfinder.utility.exception.CustomAccessDeniedHandler;
+import com.opom.jobfinder.utility.exception.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class JobFinderSecurityConfig {
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception -> {
+                    exception
+                            .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                            .accessDeniedHandler(new CustomAccessDeniedHandler());
+                })
+                .build();
+    }
+
 }
--- src/main/java/com/opom/jobfinder/utility/MessageConstants.java
+++ src/main/java/com/opom/jobfinder/utility/MessageConstants.java
@@ -7,4 +7,7 @@ public class MessageConstants {
     public static final String BAD_REQUEST_ERROR = "00400";
     public static final String INTERNAL_SERVER_ERROR = "00500";
     public static final String EMAIL_ALREADY_EXISTS = "00001";
+    public static final String UNAUTHORIZED_ERROR = "00401";
+    public static final String ACCESS_DENIED_ERROR = "00403";
+    public static final String USER_DOES_NOT_EXIST = "00002";
 }
