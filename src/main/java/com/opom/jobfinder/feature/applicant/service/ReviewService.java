44465e666e4f22fce200341dd40b43217d36c8bd
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-17 19:03:51 +0630

    modify review service with comment

--- src/main/java/com/opom/jobfinder/feature/applicant/service/ReviewService.java
+++ src/main/java/com/opom/jobfinder/feature/applicant/service/ReviewService.java
@@ -7,11 +7,22 @@ import com.opom.jobfinder.utility.BaseResponse;
 import java.util.UUID;
 
 public interface ReviewService {
-    
+
+    // ADD REVIEW
     BaseResponse addReview(Review review);
+
+    // FIND REVIEW BY ID
     BaseResponse findReviewById(String reviewId);
+
+    // UPDATE REVIEW
     BaseResponse updateReview(Review review);
+
+    // DELETE REVIEW
     BaseResponse deleteReview(String reviewId);
+
+    // GET REVIEWS BY COMPANY
     BaseResponse getReviewsByCompany(String companyId);
+
+    // GET AVERAGE REVIEW FROM COMPANY
     BaseResponse getAverageReviewFromCompany(String companyId);
 }
---
+++ src/main/java/com/opom/jobfinder/feature/applicant/service/impl/ReviewServiceImpl.java
@@ -0,0 +1,10 @@
+package com.opom.jobfinder.feature.applicant.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewServiceImpl {
+
+    // CONSTANT VALUES
+
+}
