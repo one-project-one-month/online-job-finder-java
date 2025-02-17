5138f615e25d341b47153e5a6367df6091e88229
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-17 19:06:58 +0630

    add Review Repo

--- src/main/java/com/opom/jobfinder/feature/applicant/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/opom/jobfinder/feature/applicant/service/impl/ReviewServiceImpl.java
@@ -1,10 +1,41 @@
 package com.opom.jobfinder.feature.applicant.service.impl;
 
+import com.opom.jobfinder.feature.applicant.service.ReviewService;
+import com.opom.jobfinder.model.entity.company.Review;
+import com.opom.jobfinder.utility.BaseResponse;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ReviewServiceImpl {
+public class ReviewServiceImpl implements ReviewService {
 
-    // CONSTANT VALUES
 
+    @Override
+    public BaseResponse addReview(Review review) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse findReviewById(String reviewId) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateReview(Review review) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse deleteReview(String reviewId) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse getReviewsByCompany(String companyId) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse getAverageReviewFromCompany(String companyId) {
+        return null;
+    }
 }
---
+++ src/main/java/com/opom/jobfinder/model/repo/review/ReviewRepo.java
@@ -0,0 +1,10 @@
+package com.opom.jobfinder.model.repo.review;
+
+import com.opom.jobfinder.model.entity.company.Review;
+import com.opom.jobfinder.model.repo.BaseRepository;
+
+import java.util.UUID;
+
+public interface ReviewRepo extends BaseRepository<Review, UUID> {
+
+}
