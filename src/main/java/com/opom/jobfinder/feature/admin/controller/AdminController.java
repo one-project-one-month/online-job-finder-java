822f40569058e1661f9c1caaa33a6ce57aa3e05d
Coral-Farmiga23 <thandaroo.npe2020@gmail.com>
2025-02-19 02:24:57 +0630

    skill commit

--- src/main/java/com/opom/jobfinder/feature/admin/controller/AdminController.java
+++ src/main/java/com/opom/jobfinder/feature/admin/controller/AdminController.java
@@ -1,14 +1,5 @@
 package com.opom.jobfinder.feature.admin.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
-@RestController
-@AllArgsConstructor
-@RequestMapping("/admin")
 public class AdminController {
 
 }
--- src/main/java/com/opom/jobfinder/feature/admin/service/impl/AdminServiceImpl.java
+++ src/main/java/com/opom/jobfinder/feature/admin/service/impl/AdminServiceImpl.java
@@ -1,16 +1,6 @@
 package com.opom.jobfinder.feature.admin.service.impl;
 
-import org.springframework.stereotype.Service;
+public class AdminServiceImpl{
 
-import com.opom.jobfinder.feature.admin.service.AdminService;
-
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-public class AdminServiceImpl implements AdminService {
-
-   
-   
 
 }
