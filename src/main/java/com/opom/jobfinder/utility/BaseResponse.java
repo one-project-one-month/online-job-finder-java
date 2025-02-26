d07cf1eb651fa6c8fd8d1f7300801d4a372e6830
Furry33 <mathet426@gmail.com>
2025-02-26 22:10:06 +0700

    upload 1.1

--- src/main/java/com/opom/jobfinder/utility/BaseResponse.java
+++ src/main/java/com/opom/jobfinder/utility/BaseResponse.java
@@ -2,16 +2,16 @@ package com.opom.jobfinder.utility;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public record BaseResponse (
         @JsonFormat(shape = JsonFormat.Shape.STRING,  pattern = "yyyy-MM-dd HH:mm:ss.SSS", timezone="Asia/Yangon")
-        Date timestamp,
+        LocalDateTime timestamp,
         String errorCode,
         Object data,
         String message
 ) {
     public static BaseResponse of(String errorCode, Object data, String message) {
-        return new BaseResponse(new Date(), errorCode, data, message);
+        return new BaseResponse(LocalDateTime.now(), errorCode, data, message);
     }
 }
