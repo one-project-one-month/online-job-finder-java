12718d64aa9cfa4c33ade6f6aa828272c3f6f471
devAdriam <naingaung9863@gmail.com>
2025-02-28 21:49:59 +0630

    chore(admin): remove unnecessary endpoints

--- src/main/java/com/opom/jobfinder/feature/admin/service/impl/AdminServiceImpl.java
+++ src/main/java/com/opom/jobfinder/feature/admin/service/impl/AdminServiceImpl.java
@@ -1,48 +1,16 @@
 package com.opom.jobfinder.feature.admin.service.impl;
 
-import com.opom.jobfinder.dto.info.SkillDto;
-import com.opom.jobfinder.feature.admin.service.AdminService;
-import com.opom.jobfinder.model.entity.info.Skill;
-import com.opom.jobfinder.model.repo.info.SkillRepo;
-import com.opom.jobfinder.util.info.SkillUtil;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.opom.jobfinder.feature.admin.service.AdminService;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService {
 
-    private final SkillRepo skillRepo;
+   
+   
 
-    public List<SkillDto> listAllSkills() {
-        return skillRepo
-                .findAll()
-                .stream()
-                .map(SkillUtil::skillDto)
-                .collect(Collectors.toList());
-    }
-
-    public SkillDto getSkillById(int id) {
-        Skill skill = skillRepo.findById(id).orElseThrow();
-        return SkillUtil.skillDto(skill);
-    }
-
-    public SkillDto createSkill(SkillDto skillDto) {
-        Skill skill = skillRepo.save(SkillUtil.toSkillEntity((skillDto)));
-        return SkillUtil.skillDto(skill);
-    }
-
-    public SkillDto updateSkill(SkillDto skillDto, int id) {
-        Skill skill = skillRepo.findById(id).orElseThrow();
-        skill.setName(skillDto.getName());
-        skillRepo.save(skill);
-        return SkillUtil.skillDto(skill);
-    }
-
-    public void deleteSkill(int id) {
-        skillRepo.deleteById(id);
-    }
 }
