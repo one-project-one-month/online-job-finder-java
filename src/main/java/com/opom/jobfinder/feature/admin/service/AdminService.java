4f432ea807017baaa2a6645d67e2ab77d65c2d7d
devAdriam <naingaung9863@gmail.com>
2025-02-28 21:49:59 +0630

    chore(admin): remove unnecessary endpoints

--- src/main/java/com/opom/jobfinder/feature/admin/service/AdminService.java
+++ src/main/java/com/opom/jobfinder/feature/admin/service/AdminService.java
@@ -1,15 +1,6 @@
 package com.opom.jobfinder.feature.admin.service;
 
-import com.opom.jobfinder.dto.info.SkillDto;
-
-import java.util.List;
-
 public interface AdminService{
 
-    List<SkillDto> listAllSkills();
-    SkillDto getSkillById(int id);
-    SkillDto createSkill(SkillDto skillDto);
-    SkillDto updateSkill(SkillDto skillDto, int id);
-    void deleteSkill(int id);
 
 }
