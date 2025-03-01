cf88cf0a4a93dfd806206a90c3b8175d7d422c19
ahkar <ahkarminhtut111@gmail.com>
2025-03-01 14:40:28 +0630

    fix merge errors

--- src/main/java/com/opom/jobfinder/model/entity/account/Account.java
+++ src/main/java/com/opom/jobfinder/model/entity/account/Account.java
@@ -4,19 +4,13 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.opom.jobfinder.model.entity.AbstractEntity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
